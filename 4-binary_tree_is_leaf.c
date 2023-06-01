22523d9dcfe83b195a5bf82776734ffd43a91eea
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-06-01 01:08:18 -0700

    correction

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,8 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return (0);
+		return (NULL);
 	if (node->left == NULL && node->right == NULL)
-	        return (0);
-        }
+	        return (1);
+        return (0);
 }
