3ee82b0076f2fb53fde8d685b0150fd69b7ed530
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-06-01 01:35:43 -0700

    4,5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,12 +3,12 @@
 /**
  * binary_tree_is_root - Function that checks if a node is a root
  * @node: Node to check
- * Return: 0 if is not a root 1 if it is
+ * Return: 0 if root 1
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
-		return (NULL);
+		return (0);
 	return (1);
 }
