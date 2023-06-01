dfbebab238e8167902c9c37981deac98e7bf201a
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-06-01 01:51:22 -0700

    5 . 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder - print elements of tree using pre-order traversal
+ * @tree: tree to go through
+ * @func: function to use
+ * Return: 0
+*/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
